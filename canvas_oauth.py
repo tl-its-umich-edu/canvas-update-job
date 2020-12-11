13eb542d4757f0c13aa0fe736c0512db3bdd5c9c
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2020-12-11 15:51:51 -0500

    First commit of updater job

---
+++ canvas_oauth.py
@@ -0,0 +1,56 @@
+import json
+import requests
+import logging
+from urllib.parse import urlparse, parse_qs
+
+"""
+ Step one, go to Canvas Admin->Developer Keys
+ + Developer Key, + API Key
+
+ Fill in Key name, Owner Email
+ Redirect URI (as anything, I use https://example.com/oauth_complete)
+
+ Select Enforce Scopes and pick the scopes, add these scopes here.
+
+ After saving get the client_id and key
+"""
+
+with open('oauth_settings.json') as f:
+  js = json.load(f)
+
+url_scope = "+".join(js["scopes"])
+
+# Step 1, call this with the client id, redirect_uri configured and scope 
+print(f"Go to this URL in a browser and authorize:\n{js['url']}/auth?client_id={js['client_id']}&response_type=code&redirect_uri={js['redirect_uri']}&scope={url_scope}")
+auth_response = input('Copy/Paste the full callback URL:')
+
+auth_code = parse_qs(urlparse(auth_response).query).get("code")
+
+"""
+oauth2_get = {
+        'client_id': js['client_id'],
+        'redirect_uri': js'[redirect_uri'],
+        'response_type': 'code',
+        'scope': url_scope
+        }
+"""
+
+# print (requests.get(url+"/auth", params=oauth2_get))
+# Step 2, Authorize and extract the code. Can this be automated or manual?
+
+
+# Step 3 POST back to login with code
+
+# This is the code from step 2
+oauth2_post = {
+    'grant_type': 'authorization_code',
+    'client_id': js['client_id'],
+    'client_secret': js['client_secret'],
+    'redirect_uri': js['redirect_uri'],
+    'code': auth_code,
+#    'replace_tokens': True
+    }
+
+res = (requests.post(js['url']+"/token", data=oauth2_post))
+print ("This is your token, place this in a JSON file")
+print (res.text)
---
+++ update_accounts.py
@@ -0,0 +1,49 @@
+# Updating MWrite LTI links courses and assignments in sites
+import json
+import logging
+
+from canvasapi import Canvas
+from canvasapi.exceptions import ResourceDoesNotExist, InvalidAccessToken
+import requests
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def refresh_token(api_url: str, refresh_token: str, client_id: str, client_secret: str, redirect_uri: str) -> str:
+    access_token = requests.post(api_url+"/login/oauth2/token",
+                                 data=[("grant_type", "refresh_token"),
+                                       ("refresh_token", refresh_token),
+                                       ("client_id", client_id),
+                                       ("client_secret", client_secret),
+                                       ("redirect_uri", redirect_uri)
+                                       ],
+                                 )
+
+    # Check for error, and return the token if no error
+    if access_token.status_code == requests.codes.ok:
+        return access_token.json().get("access_token")
+    else:
+        logger.error("Could not refresh access token")
+        return ""
+
+def update_discovery_url(api_url: str):
+    with open("canvas_token.json") as token_file:
+        TOKEN = json.load(token_file)
+    with open("oauth_Settings.json") as oauth_file:
+        OAUTH = json.load(oauth_file)
+
+    access_token = TOKEN.get("access_token")
+    try:
+        CANVAS = Canvas(api_url, access_token)
+        account = CANVAS.get_account(1)
+    except InvalidAccessToken:
+        logger.info("Access token is invalid, attempting to refresh!")
+        access_token = refresh_token(api_url, TOKEN.get("refresh_token"),
+                                     OAUTH.get("client_id"), OAUTH.get("client_secret"), OAUTH.get("redirect_uri"))
+        CANVAS = Canvas(api_url, access_token)
+        account = CANVAS.get_account(1)
+        print(account)
+        #    account.update_account_auth_settings(sso_settings={"auth_discovery_url":""})
+
+update_discovery_url("https://umich.beta.instructure.com")
