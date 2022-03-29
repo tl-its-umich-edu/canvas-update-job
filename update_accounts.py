7d15a93dbdf04d3074a47e04ba592eff40e8b1d1
Matthew Jones <jonespm@gmail.com>
2022-03-29 14:22:52 -0400

    Fixes #1 Make URL's configurable

--- update_accounts.py
+++ update_accounts.py
@@ -3,7 +3,7 @@
 import argparse
 import json
 import logging
-import os
+import sys
 
 from canvasapi import Canvas
 from canvasapi.exceptions import ResourceDoesNotExist, InvalidAccessToken
@@ -30,23 +30,19 @@ def refresh_token(api_url: str, refresh_token: str, client_id: str, client_secre
         log.error("Could not refresh access token")
         return ""
 
-def update_discovery_url(api_url: str, discovery_url: str):
-    with open(args.canvas_token) as token_file:
-        TOKEN = json.load(token_file)
-    with open(args.oauth_settings) as oauth_file:
-        OAUTH = json.load(oauth_file)
-
-    access_token = TOKEN.get("access_token")
+def update_discovery_url(api_url: str, discovery_url: str, token: dict, settings: dict):
+    log.info(f"Updating discovery url on {api_url} to '{discovery_url}'")
+    access_token = token.get("access_token")
     try:
-        CANVAS = Canvas(api_url, access_token)
-        account = CANVAS.get_account(1)
+        canvas = Canvas(api_url, access_token)
+        account = canvas.get_account(1)
     except InvalidAccessToken:
         log.info("Access token is invalid, attempting to refresh!")
-        access_token = refresh_token(api_url, TOKEN.get("refresh_token"),
-                                     OAUTH.get("client_id"), OAUTH.get("client_secret"), OAUTH.get("redirect_uri"))
+        access_token = refresh_token(api_url, token.get("refresh_token"),
+                                     settings.get("client_id"), settings.get("client_secret"), settings.get("redirect_uri"))
         log.debug(access_token)
-        CANVAS = Canvas(api_url, access_token)
-        account = CANVAS.get_account(1)
+        canvas = Canvas(api_url, access_token)
+        account = canvas.get_account(1)
 
     try:
         # TODO: Make this URL configurable
@@ -55,21 +51,24 @@ def update_discovery_url(api_url: str, discovery_url: str):
     except Exception as e:
         log.error(f"Error updating {api_url} {e.message}")
 
-parser = argparse.ArgumentParser(description='Update Canvas Test/Beta Settings')
-parser.add_argument('canvas_token', type=str, help='Canvas Token File')
-parser.add_argument('oauth_settings', type=str, help='OAuth Settings File')
+def main():
+    parser = argparse.ArgumentParser(description='Update Canvas Test/Beta Settings')
+    parser.add_argument('canvas_token', type=str, help='Canvas Token File')
+    parser.add_argument('settings', type=str, help='Settings File')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if not os.path.isfile(args.canvas_token):
-    log.warn("Canvas Token file does not exist, must provide the path to this file")
-    sys.exit()
-if not os.path.isfile(args.oauth_settings):
-    log.warn("OAuth Settings file does not exist, must provide the path to this file")
-    sys.exit()
+    try:
+        with open(args.canvas_token) as token_file:
+            token = json.load(token_file)
+        with open(args.settings) as settings_file:
+            settings = json.load(settings_file)
+    except IOError:
+        log.exception("Could not read one of the files provided as arguments.")
+        sys.exit()
 
-# TODO: Make this URL configurable?
-update_discovery_url("https://umich.beta.instructure.com", "")
-update_discovery_url("https://canvas-test.it.umich.edu", "https://canvas-dev.dsc.umich.edu/")
+    for canvas_server, discovery_url  in settings.get("update_discovery_urls", {}).items():
+        update_discovery_url(canvas_server, discovery_url, token, settings)
 
-
+if __name__ == '__main__':
+    main()
