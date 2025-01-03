bf727be47062acbcc84922dada8c3a01255ef245
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2025-01-03 16:41:54 -0500

    Validate json values with pydantic
    Some additional cleanup

--- update_accounts.py
+++ update_accounts.py
@@ -7,11 +7,27 @@ import sys
 
 from canvasapi import Canvas
 from canvasapi.exceptions import ResourceDoesNotExist, InvalidAccessToken
+from pydantic import BaseModel
 import requests
 
 # Set this to DEBUG for more information
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
+
+#Matches up to the oauth_settings.json file
+class OAuthSettings(BaseModel):
+    account_id: int
+    client_id: str
+    client_secret: str
+    update_discovery_urls: dict[str, str]
+
+# Matches up to the canvas_token.json file
+class Token(BaseModel):
+    refresh_token: str
+    access_token: str
+    token_type: str
+    expires_in: int
+    user: dict
 
 def refresh_token(api_url: str, refresh_token: str, client_id: str, client_secret: str, redirect_uri: str) -> str:
     access_token = requests.post(api_url+"/login/oauth2/token",
@@ -30,26 +46,26 @@ def refresh_token(api_url: str, refresh_token: str, client_id: str, client_secre
         log.error("Could not refresh access token")
         return ""
 
-def update_discovery_url(api_url: str, discovery_url: str, token: dict, settings: dict):
-    log.info(f"Updating discovery url on {api_url} to '{discovery_url}'")
-    access_token = token.get("access_token")
+def update_discovery_url(canvas_url: str, discovery_url: str, token: Token, settings: OAuthSettings):
+    log.info(f"Updating discovery url on {canvas_url} to '{discovery_url}'")
+    access_token = token.access_token
     try:
-        canvas = Canvas(api_url, access_token)
+        canvas = Canvas(canvas_url, access_token)
         account = canvas.get_account(1)
     except InvalidAccessToken:
         log.info("Access token is invalid, attempting to refresh!")
-        access_token = refresh_token(api_url, token.get("refresh_token"),
-                                     settings.get("client_id"), settings.get("client_secret"), settings.get("redirect_uri"))
+        access_token = refresh_token(canvas_url, token.refresh_token,
+                                     settings.client_id, settings.client_secret, discovery_url)
         log.debug(access_token)
-        canvas = Canvas(api_url, access_token)
-        account = canvas.get_account(1)
+        canvas = Canvas(canvas_url, access_token)
+        account = canvas.get_account(settings.account_id)
 
     try:
         # TODO: Make this URL configurable
         account.update_account_auth_settings(
             sso_settings={"auth_discovery_url": discovery_url})
     except Exception as e:
-        log.error(f"Error updating {api_url} {e.message}")
+        log.error(f"Error updating {api_url} {str(e)}")
 
 def main():
     parser = argparse.ArgumentParser(description='Update Canvas Test/Beta Settings')
@@ -60,15 +76,15 @@ def main():
 
     try:
         with open(args.canvas_token) as token_file:
-            token = json.load(token_file)
+            token = Token(**json.load(token_file))
         with open(args.settings) as settings_file:
-            settings = json.load(settings_file)
+            oauth = OAuthSettings(**json.load(settings_file))
     except IOError:
         log.exception("Could not read one of the files provided as arguments.")
         sys.exit()
 
-    for canvas_server, discovery_url  in settings.get("update_discovery_urls", {}).items():
-        update_discovery_url(canvas_server, discovery_url, token, settings)
+    for canvas_url, discovery_url in oauth.update_discovery_urls.items():
+        update_discovery_url(canvas_url, discovery_url, token, oauth)
 
 if __name__ == '__main__':
     main()
